e99ffd1985b8087908474ac920731c10f2476fc5
Damian <damian.trafialek@gmail.com>
2022-08-18 20:24:51 +0100

    added tutorial

--- gas/Navigation.js
+++ gas/Navigation.js
@@ -2,7 +2,7 @@ const onOpen = (e) => {
   SpreadsheetApp.getUi().createAddonMenu()
     .addItem('Open', 'start')  
     .addSeparator()
-    .addItem('Tutorial', tutorial)
+    .addItem('Tutorial', 'tutorial')
     .addToUi()
 }
 
@@ -10,8 +10,9 @@ const onInstall = (e) => onOpen(e)
 
 const start = () => 
   SpreadsheetApp.getUi().showSidebar( new Template('html/boilerplate',{body:'html/step1'}).evaluateWithTitle('Pixel Art Creator') ) 
+ 
+const tutorial = () => SpreadsheetApp.getUi().showModelessDialog(
+   HtmlService.createHtmlOutput( includePro_('html/boilerplate',{body:'html/tutorial',logo:false,footer:false}) ), 'Pixel Art Creator - Tutorial') 
 
-const tutorial = () => SpreadsheetApp.getUi().alert('tutorial section is still to be developed...')
-
-const thanks = (data) => SpreadsheetApp.getUi().showSidebar( new Template('html/boilerplate',{body:'html/thanks',...data}).evaluateWithTitle('Pixel Art Creator - Thanks') ) 
+const thanks = (data) => SpreadsheetApp.getUi().showSidebar(new Template('html/boilerplate',{body:'html/thanks',...data}).evaluateWithTitle('Pixel Art Creator - Thanks') ) 
  
